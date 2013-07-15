e06153fb2b1dab975ab8eec3648e045b1e7945ef
phackz <phackz88@gmail.com>
2013-07-15 10:07:40 -0400

    still writing

--- find-music.pl
+++ find-music.pl
@@ -2,13 +2,17 @@
 use v5.010;
 use strict;
 use warnings;
+
 use File::Find::Rule;
 use File::Path;
 use File::Copy;
+use Cwd;
 
 my $home1 = "/home/pman";
 my $home2 = "/home/rpothier";
 my $home_music_dir;
+
+my $remote_drive = "/run/media/pman/Seagate Expansion Drive/Music";
 
 if ( -d $home1 ) {
   $home_music_dir = "$home1/Music";
@@ -19,22 +23,28 @@ if ( -d $home1 ) {
   exit 1;
 }
 
+if ( ! -d $remote_drive ) {
+  say "Couldn't find external HDD";
+  exit 1;
+}
+
+my @files_local = grab_files($home_music_dir);
+my @files_remote = grab_files($remote_drive);
+
+foreach my $file (@files_remote){
+  say $file;     
+}
+
 sub grab_files {
   my $dir = shift;
-
+  my @return;
   my @files = File::Find::Rule->file()
 			      ->name( qr/\.(mp3|wma|ogg|wav|jpg)$/ )	
-			      ->in($dir);
+ 			      ->in($dir);
+  foreach my $file (@files) {
+    $file =~ s/$dir\///;
+    push(@return, $file);
+  }
+  return(@return);
 }
 
-sub write_file_list {
-  my $files = shift;
-
-}
-
-my @files = grab_files($home_music_dir);
-
-
-foreach my $file (@files){
-  say $file;
-}
